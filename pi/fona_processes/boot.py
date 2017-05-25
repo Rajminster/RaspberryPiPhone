8af34c917ea0a7f8615e692dcf2848583ea263d2
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-05-25 18:19:18 -0400

    Update boot.py

--- pi/fona_processes/boot.py
+++ pi/fona_processes/boot.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# boot.py
+
 from call_thread import Call_Thread
-from fona import power_gsm
 from sms_thread import SMS_Thread
 from threading import Lock
 
-"""Script to run whenever the Raspberry Pi is booted to power GSM and create
-background threads to check for when a call or message has been received."""
+__author__ = "Nikola Istvanic"
+__date__ = "2017-05-24"
+__version__ = "1.0"
+
+"""Script to run whenever the Raspberry Pi is booted to create background
+threads which check for when a call or message has been received."""
 
 if __name__ == '__main__':
-    power_gsm()
     sLock = Lock()
     Call_Thread(sLock).start()
     SMS_Thread(sLock).start()
