b9471febd051a593eae836c9d0c52beacfec6d98
Rajminster <rajminster@gmail.com>
2017-05-29 23:02:26 -0400

    Gallery can dynamically load images

--- pi/gallery/gallery.py
+++ pi/gallery/gallery.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.lang import Builder
+from kivy.logger import Logger
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
 from kivy.uix.image import AsyncImage
@@ -9,14 +10,18 @@ from kivy.uix.image import AsyncImage
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from os.path import dirname, join
+
+from setuptools.glob import glob
 
 
 class GalleryApp(App):
     def build(self):
         carousel = Carousel(direction='right')
-        for i in range(10):
-            src = "DCIM\Design.png"
-            image = AsyncImage(source=src, allow_stretch=True)
+        # get any files into images directory
+        curdir = dirname(__file__)
+        for filename in glob(join(curdir, 'DCIM', '*')):
+            image = AsyncImage(source=filename, allow_stretch=True)
             carousel.add_widget(image)
         return carousel
         #return BoxLayout()
