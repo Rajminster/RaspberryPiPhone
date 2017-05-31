550d76d74ac2605916a7bd227a0fea1703f09266
Rajminster <rajminster@gmail.com>
2017-05-30 20:54:43 -0400

    Gallery is fixed to phone res, fullscreen off

--- pi/gallery/gallery.py
+++ pi/gallery/gallery.py
@@ -4,6 +4,9 @@ from kivy.logger import Logger
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
 from kivy.uix.image import AsyncImage
+from kivy.config import Config
+from kivy.core.window import Window
+
 
 # kivy.require('1.0.6')  # replace with your current kivy version !
 
@@ -17,6 +20,8 @@ from setuptools.glob import glob
 
 class GalleryApp(App):
     def build(self):
+        Window.size = (480, 800)
+        Window.fullscreen = False
         carousel = Carousel(direction='right')
         # get any files into images directory
         curdir = dirname(__file__)
@@ -30,6 +35,7 @@ class GalleryApp(App):
 def main():
     home = GalleryApp()
     home.run()
+    Config.getint('kivy', 'show_fps')
 
 
 if  __name__ =='__main__':main()
