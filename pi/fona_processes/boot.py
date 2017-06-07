675b4b9cfd093ce1cb4da476a41a630326dcc76e
NikolaIstvanic <istvanic.nikola@gmail.com>
2017-06-06 22:15:05 -0400

    Updated for UI thread and more locks

--- pi/fona_processes/boot.py
+++ pi/fona_processes/boot.py
@@ -3,12 +3,17 @@ from sms_thread import SMS_Thread
 from ui_thread import UI_Thread
 from threading import Lock
 
+__author__ = 'Nikola Istvanic'
+__date__ = '2017-05-30'
+__version__ = '1.0'
+
 """Script to run whenever the Raspberry Pi is booted to power GSM and create
 background threads to check for when a call or message has been received."""
 
 if __name__ == '__main__':
-    sLock = Lock()
-    Call_Thread(sLock).start()
-    SMS_Thread(sLock).start()
-    UI_Thread(sLock).start() # TODO: GUI thread needs lock because different functions like
-    # answering calls/messages requires a lock
+    fona_lock = Lock()
+    call_lock = Lock()
+    sms_lock = Lock()
+    Call_Thread(fona_lock, call_lock).start()
+    SMS_Thread(fona_lock, sms_lock).start()
+    UI_Thread(fona_lock, call_lock, sms_lock).start()
