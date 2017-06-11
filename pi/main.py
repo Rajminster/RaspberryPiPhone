4820365d8242f0bc1ca063ce827cffbf317cc538
Rishi Raj <rajminster@gmail.com>
2017-06-10 21:14:45 -0400

    Imported Home Screen UI Buttons, starting to assemble home screen

--- pi/main.py
+++ pi/main.py
@@ -1,18 +1,39 @@
+from glob import glob
+from os.path import dirname, join
+
 import kivy
+from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty, StringProperty, Logger
 from kivy.uix.boxlayout import BoxLayout
 
 # kivy.require('1.0.6')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import AsyncImage
 from kivy.uix.label import Label
 
 
 class HomeScreenApp(App):
     def build(self):
-        return BoxLayout()
+        # the root is created in pictures.kv
+        root = GridLayout(cols=3)
+        Window.size = (480, 800)
+        Window.fullscreen = False
+
+        # get any files into images directory
+        curdir = dirname(__file__)
+        for filename in glob(join(curdir, 'home_UI', '*')):
+            try:
+                # load the image
+                image = AsyncImage(source=filename, allow_stretch=True)
+                # add to the main field
+                root.add_widget(image)
+            except Exception as e:
+                Logger.exception('Pictures: Unable to load <%s>' % filename)
+        return root
 
 
 if __name__ == '__main__':
