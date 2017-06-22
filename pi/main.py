95e87f809b9cbafbc4f2d4f0c74f25a0d5d4a1f8
Rajminster <rajminster@gmail.com>
2017-06-22 06:57:15 -0400

    Made all Home Screen Icons Functional Buttons

--- pi/main.py
+++ pi/main.py
@@ -11,6 +11,7 @@ from kivy.uix.boxlayout import BoxLayout
 
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import AsyncImage, Image
 from kivy.uix.label import Label
@@ -20,20 +21,43 @@ class HomeScreenApp(App):
     def build(self):
         uproot = BoxLayout()
         # the root is created in pictures.kv
-        root = GridLayout(cols=3)
-        root.spacing = [10,-300]
+        #root = GridLayout(cols=3)
+        #root.spacing = [10,-300]
+        root = FloatLayout()
         Window.size = (480, 800)
         Window.fullscreen = False
 
         # get any files into images directory
+        #75 from left, 160 between
+        x = 30
+        y = 410
+        col = 1
+
+        button = Button(size_hint=(.1, .07), pos=(218, 10))
+        button.opacity = 0.5
+        image = Image(source='Home_Button.png', allow_stretch=False, pos=(0,-365))
+        root.add_widget(button)
+        root.add_widget(image)
+
         curdir = dirname(__file__)
         for filename in glob(join(curdir, 'home_UI_icons', '*')):
             try:
                 # load the image
+                button = Button(size_hint=(.205, .122), pos=(x, y))
                 image = Image(source=filename, allow_stretch=False)
-                image.size = [0.5,0.5]
+                #image.size = [0.5,0.5]
+                button.add_widget(image)
+                image.center = (button.center_x,button.center_y)
                 # add to the main field
-                root.add_widget(image)
+                root.add_widget(button)
+                if col == 3:
+                    col = 1
+                    x = 30
+                    y -= 140
+                else:
+                    x += 160
+                    col += 1
+
             except Exception as e:
                 Logger.exception('Pictures: Unable to load <%s>' % filename)
         uproot.add_widget(root)
