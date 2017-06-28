d38483bfa3edc29dd91ded377adf2a4db5f208a6
Rajminster <rajminster@gmail.com>
2017-06-27 23:45:22 -0400

    Fixed merge

--- pi/music/music.py
+++ pi/music/music.py
@@ -1,19 +1,16 @@
-<<<<<<< HEAD
 import os
 from Canvas import Rectangle
 
 from kivy.core.window import Window
 from kivy.graphics.instructions import Canvas
 from kivy.uix.boxlayout import BoxLayout
-=======
+
 from kivy.core.audio import SoundLoader
 
 # need to define clicked in UI
->>>>>>> d0565c01038d8814199a5171e5e31ee39e20a895
+#sound = SoundLoader.load(clicked)
 
-sound = SoundLoader.load(clicked)
 
-<<<<<<< HEAD
 from kivy.app import App
 from os.path import dirname, abspath
 
@@ -30,7 +27,6 @@ class MusicApp(App):
 if __name__ == '__main__':
     home = MusicApp()
     home.run()
-=======
 if sound:
     sound.play()
->>>>>>> d0565c01038d8814199a5171e5e31ee39e20a895
+
