2e82a1080b5df3b421a98a44a81fe1c7f45fc0b6
Dharshan Ram <Dharshan@172-30-12-170.DYNAPOOL.NYU.EDU>
2017-08-07 12:18:23 -0400

    added music functionality, and compiles somewhat. need to change path variable, but should work after that

--- pi/music/music.py
+++ pi/music/music.py
@@ -12,27 +12,59 @@ from kivy.core.audio import SoundLoader
 
 
 from kivy.app import App
+from kivy.uix.carousel import Carousel
+from kivy.uix.image import AsyncImage
 from os.path import dirname, abspath
 
 from kivy.uix.floatlayout import FloatLayout
 
-class Sound(clicked):
-    sound = SoundLoader.load(clicked)
+
+
+class Sound():
+    count = 0
+    Songsplayed = 0
+    d = dict()
+    path = '/RaspberryPiPhone/songs'
+    for filename in os.listdir(path):
+        d[count] = filename
+        count = count + 1
+    sound = SoundLoader.load(d.get(0))
+
+    first = sound
+
     def play(self):
         if sound:
             sound.play()
     def pause(self):
-        sound.stop():
+        sound.stop()
     def playAtTime(self, time):
         sound.seek(time)
 
     def next(self):
         sound.unload()
+        Songsplayed = Songsplayed + 1
+        if Songsplayed > count - 1:
+            sound = first
+            Songsplayed = 0
+        else:
+            sound = SoundLoader.load(d.get(Songsplayed))
+        play()
+
+
+    def back(self):
+        sound.unload()
+        if (Songsplayed-1)%(count-1) == 0:
+            sound = SoundLoader.load(d.get(count - 1))
+        else:
+            sound = SoundLoader.load(d.get((Songsplayed-1)%(count-1)))
+        play()
+
 
 
 
 class MusicApp(App):
     def build(self):
+
         Window.size = (480, 800)
         Window.fullscreen = False
         par = dirname(dirname(abspath(__file__)))
