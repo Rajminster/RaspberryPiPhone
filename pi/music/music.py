ef4248cf5d3d1681a963376701b6efea5f5fea95
Dharshan Ram <Dharshan@172-30-12-170.DYNAPOOL.NYU.EDU>
2017-08-07 12:58:18 -0400

    added screens w/1000 buttons to test smoothness. Can't compile b/c invalid path. Change path and compile and run.

--- pi/music/music.py
+++ pi/music/music.py
@@ -6,6 +6,7 @@ from kivy.graphics.instructions import Canvas
 from kivy.uix.boxlayout import BoxLayout
 
 from kivy.core.audio import SoundLoader
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 # need to define clicked in UI
 #sound = SoundLoader.load(clicked)
@@ -24,7 +25,7 @@ class Sound():
     count = 0
     Songsplayed = 0
     d = dict()
-    path = '/RaspberryPiPhone/songs'
+    path = 'Users/Dharshan/Documents/RaspberryPiPhone/songs'
     for filename in os.listdir(path):
         d[count] = filename
         count = count + 1
@@ -60,7 +61,34 @@ class Sound():
         play()
 
 
+class ListScreen(screen):
+    def __init__(self, **kwargs):
+        super(ListScreen,self).__init__(**kwargs)
+        box1 = BoxLayout(orientation='vertical')
+        btn1 = Button(text='go to other')
+        btn1.bind(on_press=self.changer)
+        for i in 1000:
+            btn = Button(text=str('A button #', i))
+            box1.add_widget(btn)
+    def changer(self, *args):
+        self.manager.current = 'other'
 
+
+class OtherScreen(Screen):
+    def __init__(slef, **kwargs):
+        super(OtherScreen,self).__init__(**kwargs)
+        float = FloatLayout()
+        btn2 = Button(text='go to list')
+        btn2.bind(on_press=self.changer)
+
+    def changer(self, *args):
+        self.manager.current = 'list'
+
+
+
+sm = ScreenManager()
+sm.add_widget(ListScreen(name='list'))
+sm.add_widget(OtherScreen(name='other'))
 
 class MusicApp(App):
     def build(self):
@@ -68,7 +96,7 @@ class MusicApp(App):
         Window.size = (480, 800)
         Window.fullscreen = False
         par = dirname(dirname(abspath(__file__)))
-        return FloatLayout()
+        return sm
 
 if __name__ == '__main__':
     home = MusicApp()
