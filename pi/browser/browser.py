ce721fa52666742d3595f29e2c2efe25a93d1e15
Rajminster <rajminster@gmail.com>
2017-08-09 18:56:53 -0400

    Tabbed Browser is somewhat working

--- pi/browser/browser.py
+++ pi/browser/browser.py
@@ -1,7 +1,6 @@
 
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
-from cefpython3 import cefpython as cef
 import platform
 import sys
 
@@ -9,32 +8,29 @@ import sys
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
-
-
-def main():
-    check_versions()
-    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
-    cef.Initialize()
-    cef.CreateBrowserSync(url="https://www.google.com/",
-                          window_title="Hello World!")
-    cef.MessageLoop()
-    cef.Shutdown()
-
-def check_versions():
-    print("[hello_world.py] CEF Python {ver}".format(ver=cef.__version__))
-    print("[hello_world.py] Python {ver} {arch}".format(
-          ver=platform.python_version(), arch=platform.architecture()[0]))
-    assert cef.__version__ >= "55.3", "CEF Python v55.3+ required to run this"
-
+from cefbrowser import CEFBrowser
+from tabbed import TabbedCEFBrowser
 
 class BrowserApp(App):
     def build(self):
         Window.size = (480, 800)
         Window.fullscreen = False
-        return FloatLayout()
+        return TabbedCEFBrowser(
+                urls=[
+                    "http://kivy.org",
+                    "https://github.com/kivy-garden/garden.cefpython",
+                    "http://code.google.com/p/cefpython/",
+                    "http://code.google.com/p/chromiumembedded/",
+                    "about:blank",
+                ],
+                pos=(0, 0),
+                size_hint=(None, None),
+                # size=(Window.width-40, Window.height-20),
+                size=(480, 800),
+            )
 
 if __name__ == '__main__':
-    #home = BrowserApp()
-    #home.run()
-    main()
+    home = BrowserApp()
+    home.run()
+
 
---
+++ pi/browser/cefbrowser/minimal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+"""
+Minimal example of the CEFBrowser widget use. Here you don't have any controls
+(back / forth / reload) or whatsoever. Just a kivy app displaying the
+chromium-webview.
+"""
+
+
+from kivy.app import App
+from cefbrowser import CEFBrowser
+
+
+if __name__ == '__main__':
+    class SimpleBrowserApp(App):
+        def build(self):
+            return CEFBrowser(url="http://jegger.ch/datapool/app/test_input.html")
+
+    SimpleBrowserApp().run()
