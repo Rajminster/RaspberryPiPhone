c997462152a179a98a070554fda19a182a5b3f6c
Rajminster <rajminster@gmail.com>
2017-08-09 18:35:47 -0400

    Added CEFBrowser code. Will modifiy to work on mobile

--- pi/browser/browser.py
+++ pi/browser/browser.py
@@ -1,12 +1,30 @@
 
 from kivy.core.window import Window
 from kivy.uix.boxlayout import BoxLayout
+from cefpython3 import cefpython as cef
+import platform
+import sys
 
 # kivy.require('1.0.6')  # replace with your current kivy version !
 
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 
+
+def main():
+    check_versions()
+    sys.excepthook = cef.ExceptHook  # To shutdown all CEF processes on error
+    cef.Initialize()
+    cef.CreateBrowserSync(url="https://www.google.com/",
+                          window_title="Hello World!")
+    cef.MessageLoop()
+    cef.Shutdown()
+
+def check_versions():
+    print("[hello_world.py] CEF Python {ver}".format(ver=cef.__version__))
+    print("[hello_world.py] Python {ver} {arch}".format(
+          ver=platform.python_version(), arch=platform.architecture()[0]))
+    assert cef.__version__ >= "55.3", "CEF Python v55.3+ required to run this"
 
 
 class BrowserApp(App):
@@ -16,5 +34,7 @@ class BrowserApp(App):
         return FloatLayout()
 
 if __name__ == '__main__':
-    home = BrowserApp()
-    home.run()
+    #home = BrowserApp()
+    #home.run()
+    main()
+
