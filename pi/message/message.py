0e8b2a3ac62481f8342151a711874c29d7bceac0
Rajminster <rajminster@gmail.com>
2017-08-09 20:32:34 -0400

    Message app has list of contact threads

--- pi/message/message.py
+++ pi/message/message.py
@@ -5,15 +5,31 @@ from kivy.uix.boxlayout import BoxLayout
 # kivy.require('1.0.6')  # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.listview import ListView
+from kivy.base import runTouchApp
 
 
+class MainView(ListView):
+    def __init__(self, **kwargs):
+        super(MainView, self).__init__(
+            item_strings=[str(index) for index in range(100)])
 class MessageApp(App):
     def build(self):
+        root = FloatLayout()
         Window.size = (480, 800)
         Window.fullscreen = False
-        return FloatLayout()
+        layout = BoxLayout(orientation='vertical', size=(480,700), size_hint=(None, None), pos = (0,70))
+        for i in range(12):
+            layout.add_widget(Button(text='Contact ' + str(i)))
+        root.add_widget(layout)
+        return root
 
 if __name__ == '__main__':
     home = MessageApp()
     home.run()
+    #runTouchApp(MainView())
+
+
+
