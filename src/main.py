b2dedb8fa386e70493e14a60c39cfca166a80a9a
Rajminster <rajminster@gmail.com>
2018-01-29 22:13:26 -0500

    Screen switching test work with text app on home screen

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from glob import glob
 from os.path import dirname, join
 
 import kivy
+from kivy.clock import mainthread
 from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty, Logger
@@ -18,53 +19,72 @@ from kivy.graphics import *
 from apps.gallery.gallery import GalleryApp
 from apps.message.message import MessageApp
 import multiprocessing
+from kivy.uix.screenmanager import ScreenManager, Screen, SwapTransition, CardTransition
 
-class HomeScreenApp(App):
+
+class ScreenManagement(ScreenManager):
+    ScreenManager.transition = CardTransition()
+    pass
+
+
+class Home(Screen):
+    def __init__(self, **kwargs):
+        super(Home, self).__init__(**kwargs)
+        self.on_enter()
+
+    def switch(self):
+        # here you can insert any python logic you like
+        self.parent.current = 'Test'
+
+    @mainthread
+    def on_enter(self):
+        layout = self.ids.list
+        image = Image(source='resources/Home_Button.png', allow_stretch=False, pos=(0, -365))
+        button = HomeButton(size_hint=(.1, .06), pos=(216, 12))
+        button.opacity = 0.5
+        layout.add_widget(button)
+        layout.add_widget(image)
+
+
+class Test(Screen):
+    def __init__(self, **kwargs):
+        super(Test, self).__init__(**kwargs)
+        self.on_enter()
+
+    def switch(self):
+        # here you can insert any python logic you like
+        self.parent.current = 'Home'
+
+    @mainthread
+    def on_enter(self):
+        layout = self.ids.convo
+        image = Image(source='resources/Home_Button.png', allow_stretch=False, pos=(0, -365))
+        button = HomeButton(size_hint=(.1, .06), pos=(216, 12))
+        button.opacity = 0.5
+        layout.add_widget(button)
+        layout.add_widget(image)
+
+
+class HomeScreen(App):
     def build(self):
-        Builder.load_file('HomeScreen.kv')
-        uproot = BoxLayout()
+        # Builder.load_file('HomeScreen.kv')
         root = FloatLayout()
         Window.size = (480, 800)
         Window.fullscreen = False
-        #Window.borderless = True
+        # Window.borderless = True
 
         # get any files into images directory
-        #75 from left, 160 between
+        # 75 from left, 160 between
         x = 30
         y = 395
         col = 1
 
-        image = Image(source='resources/Home_Button.png', allow_stretch=False, pos=(0,-365))
-        button = HomeButton(size_hint=(.1,.06), pos=(216, 12))
+        image = Image(source='resources/Home_Button.png', allow_stretch=False, pos=(0, -365))
+        button = HomeButton(size_hint=(.1, .06), pos=(216, 12))
         button.opacity = 0.5
         root.add_widget(button)
         root.add_widget(image)
-
-        # curdir = dirname(__file__)
-        # for filename in glob(join(curdir, 'resources/home_UI_icons', '*')):
-        #     try:
-        #         # load the image
-        #         button = Button(size_hint=(.23, .132), pos=(x, y))
-        #         image = Image(source=filename, allow_stretch=False)
-        #         image.size = (120,120)
-        #         button.add_widget(image)
-        #         image.center = (button.center_x,button.center_y)
-        #         # add to the main field
-        #         root.add_widget(button)
-        #         if col == 3:
-        #             col = 1
-        #             x = 30
-        #             y -= 140
-        #         else:
-        #             x += 160
-        #             col += 1
-        #
-        #     except Exception as e:
-        #         Logger.exception('Pictures: Unable to load <%s>' % filename)
-        #uproot.add_widget(root)
-        return root
-    def pout(self):
-        print ("Success")
+        return ScreenManagement()
 
 
 class HomeButton(Button):
@@ -72,14 +92,15 @@ class HomeButton(Button):
         super(HomeButton, self).__init__(**kwargs)
 
     def on_press(self):
-        app = MessageApp()
-        p = multiprocessing.Process(target=app.run)
-        p.start()
+        # app = MessageApp()
+        # p = multiprocessing.Process(target=app.run)
+        # p.start()
+        pass
 
     def on_release(self):
         pass
 
 
 if __name__ == '__main__':
-    home = HomeScreenApp()
+    home = HomeScreen()
     home.run()
